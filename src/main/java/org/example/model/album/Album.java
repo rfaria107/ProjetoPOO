296f139413012993a0aa156b0e2bc6fd3e96bc41
Rui Faria <alexfaria10.2005@gmail.com>
2025-04-25 02:34:41 +0100

    alguns unit tests da classe Album

--- src/main/java/org/example/model/album/Album.java
+++ src/main/java/org/example/model/album/Album.java
@@ -3,6 +3,11 @@ package org.example.model.album;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a music album containing a collection of songs and associated metadata.
+ * The album includes details such as title, artist, release year, genre,
+ * and provides functionality to manage the list of songs.
+ */
 public class Album {
     private final String title;
     private final String artist;
@@ -20,8 +25,8 @@ public class Album {
     }
 
 
-    public Song addSong(String name, String publisher, String lyrics, String musicalNotes,
-                         String genre, int durationInSeconds) {
+    public void addSong(String name, String publisher, String lyrics, String musicalNotes,
+                        String genre, int durationInSeconds) {
         if (name == null || name.isEmpty()) {
             throw new IllegalArgumentException("Song name cannot be null or empty.");
         }
@@ -32,10 +37,8 @@ public class Album {
             throw new IllegalArgumentException("A song with the same name already exists in the album.");
         }
 
-        // Create and add song to the Album
         Song song = new Song(name, this.artist, publisher, lyrics, musicalNotes, genre, durationInSeconds);
         songs.add(song);
-        return song;
     }
 
 
