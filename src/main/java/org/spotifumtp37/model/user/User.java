f5b664a086e1a2b95c64f0dff80a38b9db47ea70
rita <a104261@alunos.uminho.pt>
2025-04-27 18:29:30 +0100

    user class update

--- src/main/java/org/spotifumtp37/model/user/User.java
+++ src/main/java/org/spotifumtp37/model/user/User.java
@@ -8,16 +8,37 @@ public abstract class User {
     private final String email;
     private final String address;
     private SubscriptionPlan subscriptionplan;
-
+    private final String password;
+    private double pontos;
     //teste commit
     //  private String nome2;
 //}
 
-    public User(String name, String email, String address, SubscriptionPlan subscriptionPlan) {
+    public User(String name, String email, String address, SubscriptionPlan subscriptionPlan, String password, double pontos) {
         this.name = name;
         this.email = email;
         this.address = address;
         this.subscriptionplan = subscriptionPlan;
+        this.password = password;
+        this.pontos = pontos;
+    }
+
+    public User(User other) {
+        this.name = other.getName();
+        this.email = other.getEmail();
+        this.address = other.getAddress();
+        this.subscriptionplan = other.getSubscriptionPlan();
+        this.password = other.getPassword();
+        this.pontos = other.getPontos();
+    }
+
+    public User() {
+        this.name = "";
+        this.email = "";
+        this.address = "";
+        this.subscriptionplan = new FreePlan();
+        this.password = "";
+        this.pontos = 0;
     }
 
     public String getName() {
@@ -36,6 +57,14 @@ public abstract class User {
         return subscriptionplan;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public double getPontos() {
+        return pontos;
+    }
+
     public void setSubscriptionPlan(SubscriptionPlan newPlan) {
         if (newPlan != null) {
             this.subscriptionplan = newPlan;  // Update the user's subscription plan with the new one
@@ -44,4 +73,13 @@ public abstract class User {
         }
     }
 
+    public void somarPontos() {
+        double newPontos = subscriptionplan.adicionaPontos(pontos); // Get the new points from the plan
+        setPontos(newPontos);
+    }
+
+    public void setPontos(double pontos) {
+        this.pontos = pontos;
+    }
 }
+
