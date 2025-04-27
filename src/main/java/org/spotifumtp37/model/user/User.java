d0b7579b3451128ce0197628d2ce46ecf67d871a
rita <a104261@alunos.uminho.pt>
2025-04-27 02:28:19 +0100

    user class changes

--- src/main/java/org/spotifumtp37/model/user/User.java
+++ src/main/java/org/spotifumtp37/model/user/User.java
@@ -1,7 +1,47 @@
 package org.spotifumtp37.model.user;
 
+import org.spotifumtp37.model.subscription.FreePlan;
+import org.spotifumtp37.model.subscription.SubscriptionPlan;
+
 public abstract class User {
-    private int nome;
+    private final String name;
+    private final String email;
+    private final String address;
+    private SubscriptionPlan subscriptionplan;
+
     //teste commit
-    private String nome2;
+    //  private String nome2;
+//}
+
+    public User(String name, String email, String address, SubscriptionPlan subscriptionPlan) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.subscriptionplan = subscriptionPlan;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public SubscriptionPlan getSubscriptionPlan() {
+        return subscriptionplan;
+    }
+
+    public void setSubscriptionPlan(SubscriptionPlan newPlan) {
+        if (newPlan != null) {
+            this.subscriptionplan = newPlan;  // Update the user's subscription plan with the new one
+        } else {
+            throw new IllegalArgumentException("Subscription plan cannot be null.");
+        }
+    }
+
 }
